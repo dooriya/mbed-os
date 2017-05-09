9c49258791d454c17f093b2b6c5a487319e4197b
lizhibo32 <17602115406@163.com>
2017-05-09 13:50:59 +0800

    build project.

--- targets/TARGET_MXCHIP/TARGET_MK3239/system_stm32f4xx.c
+++ targets/TARGET_MXCHIP/TARGET_MK3239/system_stm32f4xx.c
@@ -83,7 +83,7 @@
 #include "stm32f4xx.h"
 #include "hal_tick.h"
 
-#if !defined  (HSE_VALUE) 
+#if !defined  (HSE_VALUE)
   #define HSE_VALUE    ((uint32_t)8000000) /*!< Default value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
